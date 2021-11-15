f8f1eda6fcabb259fb4408fbe95f8e69f9cc8a72
Fs02 <surya.asriadie@gmail.com>
2021-11-15 23:44:13 +0900

    init

---
+++ embulk-parser-flexml.gemspec
@@ -0,0 +1,22 @@
+lib = File.expand_path("../lib", __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+Gem::Specification.new do |spec|
+  spec.name          = "embulk-parser-flexml"
+  spec.version       = "0.13.0"
+  spec.authors       = ["Surya Asriadie"]
+  spec.email         = ["surya.asriadie@gmail.com"]
+  spec.summary       = %q{Flexible Embulk parser plugin for XML}
+  spec.description   = %q{XML parser plugin is Embulk plugin to fetch entries in xml format. Supports xpath and attributes}
+  spec.homepage      = "https://github.com/Fs02/embulk-parser-flexml"
+  spec.license       = "MIT"
+
+  spec.files         = `git ls-files -z`.split("\x0")
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.require_paths = ["lib"]
+
+  spec.add_dependency "rexml", "~> 3.1.9"
+  spec.add_development_dependency "bundler", "~> 1.0"
+  spec.add_development_dependency 'embulk', ['>= 0.9.15']
+  spec.add_development_dependency "rake", "~> 10.0"
+end
---
+++ lib/embulk/parser/flexml.rb
@@ -0,0 +1,91 @@
+require 'rexml/document'
+
+module Embulk
+  module Parser
+    include REXML
+
+    class Flexml < ParserPlugin
+      Plugin.register_parser("xml3", self)
+
+      def self.transaction(config, &control)
+        schema = config.param("schema", :array)
+        schema_serialized = schema.inject({}) do |memo, s|
+          memo[s["name"]] = s
+          if s["type"] == "timestamp"
+            memo[s["name"]].merge!({
+              "format" => s["format"],
+              "timezone" => s["timezone"] || "+0900"
+            })
+          end
+          memo
+        end.to_h
+        task = {
+          :schema => schema_serialized,
+          :root => config.param("root", :string)
+        }
+        columns = schema.each_with_index.map do |c, i|
+          Column.new(i, c["name"], c["type"].to_sym)
+        end
+        yield(task, columns)
+      end
+
+      def run(file_input)
+        while file = file_input.next_file
+          begin
+            xml_text = file.read
+            doc = Document.new(xml_text)
+            doc.elements.each(@task[:root]) do |e|
+              values = @task[:schema].map do |f, c|
+                row = if c.has_key?("xpath")
+                    XPath.first(e, c["xpath"])
+                  elsif c.has_key?("element")
+                    e.element(c["element"])
+                  else
+                    e
+                  end
+
+                val = if c.has_key?("attribute")
+                  row.attributes[c["attribute"]]
+                else
+                  row.text
+                end
+                convert(val, c)
+              end
+
+              @page_builder.add(values)
+            end
+          rescue Exception => e
+            puts "=>>>> ERROR: #{e}"
+          end
+        end
+
+        @page_builder.finish
+      end
+
+      def convert(val, config)
+        v = val.nil? ? "" : val
+        case config["type"]
+        when "string"
+          v
+        when "long"
+          v.to_i
+        when "double"
+          v.to_f
+        when "boolean"
+          ["yes", "true", "1"].include?(v.downcase)
+        when "timestamp"
+          unless v.empty?
+            dest = Time.strptime(v, config["format"])
+            utc_offset = dest.utc_offset
+            zone_offset = Time.zone_offset(config["timezone"])
+            dest.localtime(zone_offset) + utc_offset - zone_offset
+          else
+            nil
+          end
+        else
+          raise "Unsupported type '#{type}'"
+        end
+      end
+    end
+  end
+end
